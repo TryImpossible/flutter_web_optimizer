1489d17e11de2d4675482cf213e38cf36a22e2cf
xiebin <865068275@qq.com>
2022-11-20 15:49:27 +0800

    创建flutter_web_optimizer工程

---
+++ bin/flutter_web_optimizer.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_web_optimizer/flutter_web_optimizer.dart' as flutter_web_optimizer;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter_web_optimizer.calculate()}!');
+}
---
+++ test/flutter_web_optimizer_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_web_optimizer/flutter_web_optimizer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
