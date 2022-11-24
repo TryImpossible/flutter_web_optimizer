3ff50872b71092926f3ee0bf6fa673112a2afb63
xiebin <865068275@qq.com>
2022-11-24 20:43:08 +0800

    格式化文件

--- lib/executable.dart
+++ lib/executable.dart
@@ -3,19 +3,19 @@ import 'package:args/command_runner.dart';
 import 'src/optimize/optimize_command.dart';
 import 'src/plugin/plugin_command.dart';
 
-final _mockOptimizeCmdArgs = [
-  'optimize',
-  '--asset-base',
-  'http://192.168.101.93:9091/app/',
-  '--plugin',
-  'scripts/cos_upload_plugin.dart',
-];
-
-final _mockPluginCmdArgs = [
-  'plugin',
-  '--path',
-  'scripts/cos_upload_plugin.dart',
-];
+// final _mockOptimizeCmdArgs = [
+//   'optimize',
+//   '--asset-base',
+//   'http://192.168.101.93:9091/app/',
+//   '--plugin',
+//   'scripts/cos_upload_plugin.dart',
+// ];
+//
+// final _mockPluginCmdArgs = [
+//   'plugin',
+//   '--path',
+//   'scripts/cos_upload_plugin.dart',
+// ];
 
 void main(List<String> arguments) {
   CommandRunner(
