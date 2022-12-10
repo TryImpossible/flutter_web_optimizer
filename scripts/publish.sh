ca3fd9e92aec03309e7f6f274b9268c56bfcc26a
xiebin <865068275@qq.com>
2022-12-10 20:02:51 +0800

    修复pub get失败的问题

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,6 +4,6 @@ export https_proxy=$http_proxy;
 # set http_proxy=http://127.0.0.1:1087
 # set https_proxy=https://127.0.0.1:1087
 
-flutter pub publish --dry-run -v --server=https://pub.dartlang.org
+flutter pub publish --dry-run --server=https://pub.dartlang.org
 flutter pub publish --server=https://pub.dartlang.org
 # flutter packages pub publish --server=https://pub.dartlang.org
