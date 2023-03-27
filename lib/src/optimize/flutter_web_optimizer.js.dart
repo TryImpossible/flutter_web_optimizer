c9a571df4d8680e57436b269102a56610cc6ab90
xiebin <865068275@qq.com>
2023-03-27 18:05:43 +0800

    opt:_definePropert中set方法遍历hashfile时，判断是否字符串类型

--- lib/src/optimize/flutter_web_optimizer.js.dart
+++ lib/src/optimize/flutter_web_optimizer.js.dart
@@ -20,11 +20,13 @@ const String flutterWebOptimizerSourceCode = r'''
           },
           set: function (v) {
             value = v;
-            if (v.startsWith('./')) {
-              v = v.replace('./', '');
-            }
-            if (hashFileManifest[v]) {
-              value = assetBase + hashFileManifest[v];
+            if (typeof v === 'string') {
+              if (v.startsWith('./')) {
+                v = v.replace('./', '');
+              }
+              if (hashFileManifest[v]) {
+                value = assetBase + hashFileManifest[v];
+              }
             }
             element.setAttribute(property, value);
           }
